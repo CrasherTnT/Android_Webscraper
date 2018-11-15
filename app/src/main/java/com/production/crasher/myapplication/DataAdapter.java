82391aaebc7173b033ade385ca4c99634d51fcde
Sherwyne <sherwyne123+work@gmail.com>
2018-11-15 09:58:36 +0800

    80% DONE!!! Next To-Do List: Image and UX

--- app/src/main/java/com/production/crasher/myapplication/DataAdapter.java
+++ app/src/main/java/com/production/crasher/myapplication/DataAdapter.java
@@ -14,15 +14,13 @@ import java.util.ArrayList;
 
 public class DataAdapter extends RecyclerView.Adapter<DataAdapter.MyViewHolder> {
     private ArrayList<String> newsTitle;
-    private ArrayList<String> newsAuthor;
     private Activity mActivity;
     private ArrayList<String> newsUrl;
 
-    public DataAdapter(MainActivity activity, ArrayList<String> newsTitle, ArrayList<String> newsAuthor, ArrayList<String> newsUrl) {
+    public DataAdapter(MainActivity activity, ArrayList<String> newsTitle, ArrayList<String> newsUrl) {
         this.mActivity = activity;
         this.newsTitle = newsTitle;
         this.newsUrl = newsUrl;
-        this.newsAuthor = newsAuthor;
     }
 
     @Override
@@ -36,17 +34,15 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.MyViewHolder> 
     @Override
     public void onBindViewHolder(MyViewHolder holder, final int position) {
         holder.tv_blog_title.setText(newsTitle.get(position));
-        holder.tv_blog_author.setText(newsAuthor.get(position));
-        holder.tv_blog_url.setText(newsUrl.get(position));
         holder.setItemClickListener(new ItemClickListener() {
             @Override
             public void onClick(View view, int position, boolean isLongClick) {
                 if(isLongClick){
-                    Toast.makeText(mActivity, "Long" + newsUrl.get(position), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mActivity, newsUrl.get(position), Toast.LENGTH_SHORT).show();
                 }
                 else{
                     Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(newsUrl.get(position))); //This is used to give intent the command to open the url
+                    i.setData(Uri.parse(newsUrl.get(position))); //parsing the url
                     mActivity.startActivity(i);
                 }
             }
@@ -55,18 +51,16 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.MyViewHolder> 
 
     @Override
     public int getItemCount() {
-        return 10;
+        return 10; //To limit the list of news to display
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
-        private TextView tv_blog_title, tv_blog_author, tv_blog_url;
+        private TextView tv_blog_title;
         private ItemClickListener itemClickListener;
 
         public MyViewHolder(View view) {
             super(view);
             tv_blog_title = view.findViewById(R.id.row_tv_blog_title);
-            tv_blog_author = view.findViewById(R.id.row_tv_blog_author);
-            tv_blog_url = view.findViewById(R.id.row_tv_blog_upload_date);
             itemView.setOnLongClickListener(this);
             itemView.setOnClickListener(this);
         }
@@ -83,7 +77,7 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.MyViewHolder> 
         @Override
         public boolean onLongClick(View v) {
             itemClickListener.onClick(v, getAdapterPosition(),true);
-            return false;
+            return true;
         }
     }
 }
--- app/src/main/java/com/production/crasher/myapplication/MainActivity.java
+++ app/src/main/java/com/production/crasher/myapplication/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
                                     "https://www.reuters.com/news/archive/cybersecurity",
                                     "https://www.securitymagazine.com/topics/2236-cyber-security-news"
                                     };
-    private ArrayList<String> newsAuthor = new ArrayList<>();
     private ArrayList<String> newsTitle = new ArrayList<>();
     private ArrayList<String> newsUrl = new ArrayList<>();
     private PullRefreshLayout swipeRefreshLayout;
@@ -47,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         swipeRefreshLayout.setRefreshing(false);
                     }
-                }, 1000);
+                }, 2000);
             }
         });
     }
@@ -78,8 +77,6 @@ public class MainActivity extends AppCompatActivity {
                         Elements mElementDataSize = mBlogDocument.select("div[class=clear home-right]");
                         int mElementSize = mElementDataSize.size() - 7;
                         for (int i = 0; i < mElementSize; i++) {
-                            Elements mElementAuthorName = mBlogDocument.select("div[class=item-label]").select("span").eq(i);
-                            String mAuthorName = mElementAuthorName.text();
 
                             Elements mElementBlogTitle = mBlogDocument.select("h2[class=home-title]").eq(i);
                             String mBlogTitle = mElementBlogTitle.text();
@@ -87,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
                             Elements mElementUrl = mBlogDocument.select("a.story-link").eq(i);
                             String mUrl = mElementUrl.attr("abs:href");
 
-                            newsAuthor.add(" ");
                             newsTitle.add(mBlogTitle);
                             newsUrl.add(mUrl);
                         }
@@ -101,14 +97,11 @@ public class MainActivity extends AppCompatActivity {
                         for (int a = 0; a < mElementSize; a++ ){
                             Elements mNewsUrl = mBlogDocument.select("div[class=story-content]").select("a").eq(a);
                             Elements mNewsTitle = mBlogDocument.select("div[class=story-content]").select("h3").eq(a);
-                            Elements mNewsDate = mBlogDocument.select("span[class=timestamp]").eq(a);
 
                             String mTitle = mNewsTitle.text();
                             String mUrl = mNewsUrl.attr("abs:href");
-                            String mDate = mNewsDate.text();
 
                             newsTitle.add(mTitle);
-                            newsAuthor.add(mDate);
                             newsUrl.add(mUrl);
                         }
                     }
@@ -119,14 +112,11 @@ public class MainActivity extends AppCompatActivity {
                         for (int a = 0; a < mElementSize; a++) {
                             Elements mNewsUrl = mBlogDocument.select("div[class=image]").select("a").eq(a);
                             Elements mNewsTitle = mBlogDocument.select("div[class=image]").select("a").eq(a);
-                            Elements mNewsDate = mBlogDocument.select("div[class=post-meta]").select("div[class=date article-summary__post-date]").eq(a);
 
                             String mTitle = mNewsTitle.attr("title");
                             String mUrl = mNewsUrl.attr("abs:href");
-                            String mDate = mNewsDate.text();
 
                             newsTitle.add(mTitle);
-                            newsAuthor.add(mDate);
                             newsUrl.add(mUrl);
                         }
                     }
@@ -143,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
             // Set description into TextView
 
             RecyclerView mRecyclerView = findViewById(R.id.act_recyclerview);
-            DataAdapter mDataAdapter = new DataAdapter(MainActivity.this, newsTitle, newsAuthor, newsUrl);
+            DataAdapter mDataAdapter = new DataAdapter(MainActivity.this, newsTitle, newsUrl);
             RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
             mRecyclerView.setLayoutManager(mLayoutManager);
             mRecyclerView.setAdapter(mDataAdapter);
