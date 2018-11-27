e3da1733a7743e7f4234fa0998985f91e29a7f36
Sherwyne <sherwyne123+work@gmail.com>
2018-11-27 21:02:24 +0800

    Notification added

--- app/src/main/java/com/production/crasher/myapplication/MainActivity.java
+++ app/src/main/java/com/production/crasher/myapplication/MainActivity.java
@@ -1,9 +1,17 @@
 package com.production.crasher.myapplication;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -31,6 +39,24 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> newsUrl = new ArrayList<>();
     private ArrayList<String> newsImage = new ArrayList<>();
     private PullRefreshLayout swipeRefreshLayout;
+    long startTime = 0;
+    private String stringTimer;
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+
+            stringTimer = String.format("%d:%02d", minutes, seconds);
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
 
 
     @Override
@@ -38,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new NewsGatherer().execute();
-
         swipeRefreshLayout = findViewById(R.id.swipeRefresh);
         swipeRefreshLayout.setOnRefreshListener(new PullRefreshLayout.OnRefreshListener() {
             @Override
@@ -47,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         swipeRefreshLayout.setRefreshing(false);
+
                     }
                 }, 2000);
             }
@@ -61,8 +87,8 @@ public class MainActivity extends AppCompatActivity {
             mProgressDialog.setTitle("Hackuna News");
             mProgressDialog.setMessage("Loading...");
             mProgressDialog.setIndeterminate(false);
+            startService();
             mProgressDialog.show();
-
         }
 
         @Override
@@ -73,41 +99,22 @@ public class MainActivity extends AppCompatActivity {
                     String randomUrl = resourceUrl[urlCount];
                     Document mBlogDocument = Jsoup.connect(randomUrl).get();
 
-                    if (randomUrl == resourceUrl[0]) {
-                        firstResource(mBlogDocument); //1st URL
-                        //This resource (The Hacker News) encoded their images using base64...
-                        // I'm still finding a solution to get the raw url but for now I put a temporary image
+                    switch (randomUrl){
+                        case "https://thehackernews.com/":
+                            firstResource(mBlogDocument);
+                            break;
+                        case "https://www.reuters.com/news/archive/cybersecurity":
+                            secondResource(mBlogDocument);
+                            break;
+                        case "https://www.securitymagazine.com/topics/2236-cyber-security-news":
+                            thirdResource(mBlogDocument);
+                            break;
+                        default:
+                            break;
+
                     }
-                    else if (randomUrl == resourceUrl[1]) {
-                        secondResource(mBlogDocument); //2nd URL
-                    }
-                    else if (randomUrl == resourceUrl[2]) {
-                        thirdResource(mBlogDocument); //3rd URL
-                    }
-                    /*
-                    else if (randomUrl == resourceUrl[3]){
-                        fourthResource(mBlogDocument);
-                    }
-                    else if (randomUrl == resourceUrl[4]){
-                        fifthResource(mBlogDocument);
-                    }
-                    else if (randomUrl == resourceUrl[5]){
-                        sixthResource(mBlogDocument);
-                    }
-                    else if (randomUrl == resourceUrl[6]){
-                        seventhResource(mBlogDocument);
-                    }
-                    else if (randomUrl == resourceUrl[7]){
-                        eightResource(mBlogDocument);
-                    }
-                    else if (randomUrl == resourceUrl[8]){
-                        ninethResource(mBlogDocument);
-                    }
-                    else if (randomUrl == resourceUrl[9]){
-                        tenthResource(mBlogDocument);
-                    }
-                    */
                 }
+            hackunaNotification(newsUrl.get(0), newsTitle.get(0));
             }
             catch (IOException e) {
                 e.printStackTrace();
@@ -128,7 +135,6 @@ public class MainActivity extends AppCompatActivity {
             mProgressDialog.dismiss();
         }
     }
-
     private void firstResource(Document mBlogDocument) {
         Elements mElementDataSize = mBlogDocument.select("div[class=clear home-right]");
         int mElementSize = mElementDataSize.size() - 7;
@@ -138,13 +144,13 @@ public class MainActivity extends AppCompatActivity {
             Elements mElementUrl = mBlogDocument.select("a.story-link").eq(i);
             Elements mElementImage = mBlogDocument.select("img").eq(i);
 
-            String mBlogTitle = mElementBlogTitle.text();
-            String mUrl = mElementUrl.attr("abs:href");
+            final String mBlogTitle = mElementBlogTitle.text();
+            final String mUrl = mElementUrl.attr("abs:href");
             String mImage = mElementImage.attr("alt");
 
             newsTitle.add(mBlogTitle);
             newsUrl.add(mUrl);
-            newsImage.add("https://jeffreymannfinejewelers.com/static/images/temp-inventory-landing.jpg");
+            newsImage.add("https://1.bp.blogspot.com/-AaptImXE5Y4/WzjvqBS8HtI/AAAAAAAAxSs/BcCIwpWJszILkuEbDfKZhxQJwOAD7qV6ACLcBGAs/s728-e100/the-hacker-news.jpg");
         }
     }
     private void secondResource(Document mBlogDocument){
@@ -184,139 +190,34 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-    /*
-    private void fourthResource(Document mBlogDocument){
-        Elements mElementDataSize = mBlogDocument.select("div[class=article]");
-        int mElementSize = mElementDataSize.size();
 
-        for (int i = 0; i < mElementSize; i++) {
-            Elements mNewsUrl = mBlogDocument.select("div[class=promo-headline]").select("h3").select("a").eq(i);
-            Elements mNewsTitle = mBlogDocument.select("div[class=promo-headline]").select("h3").select("a").eq(i);
-            Elements mElementImage = mBlogDocument.select("div[class=promo-image]").select("img").eq(i);
+    public void hackunaNotification(String mUrl, String mTitle){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(newsUrl.get(0)));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
-            String mTitle = mNewsTitle.text();
-            String mUrl = mNewsUrl.attr("abs:href");
-            String mImage = mElementImage.attr("src");
-
-            newsTitle.add(mTitle);
-            newsUrl.add(mUrl);
-            newsImage.add(mImage);
-
-        }
+        NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(this)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background))
+                .setContentTitle(mTitle)
+                .setContentText(mUrl)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(2, notifBuilder.build());
     }
-    private void fifthResource(Document mBlogDocument){
-        Elements mElementDataSize = mBlogDocument.select("article[class=record article-summary]");
-        int mElementSize = mElementDataSize.size();
 
-        for (int i = 0; i < mElementSize; i++) {
-            Elements mNewsUrl = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mNewsTitle = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mElementImage = mBlogDocument.select("div[class=image]").select("img").eq(i);
+    public void startService() {
+        timerHandler.postDelayed(timerRunnable, 0);
+        Intent serviceIntent = new Intent(this, ForegroundService.class);
+        serviceIntent.putExtra("inputExtra", stringTimer);
 
-            String mTitle = mNewsTitle.attr("title");
-            String mUrl = mNewsUrl.attr("abs:href");
-            String mImage = mElementImage.attr("src");
-
-            newsTitle.add(mTitle);
-            newsUrl.add(mUrl);
-            newsImage.add(mImage);
-
-        }
+        ContextCompat.startForegroundService(this, serviceIntent);
     }
-    private void sixthResource(Document mBlogDocument){
-        Elements mElementDataSize = mBlogDocument.select("article[class=record article-summary]");
-        int mElementSize = mElementDataSize.size();
 
-        for (int i = 0; i < mElementSize; i++) {
-            Elements mNewsUrl = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mNewsTitle = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mElementImage = mBlogDocument.select("div[class=image]").select("img").eq(i);
-
-            String mTitle = mNewsTitle.attr("title");
-            String mUrl = mNewsUrl.attr("abs:href");
-            String mImage = mElementImage.attr("src");
-
-            newsTitle.add(mTitle);
-            newsUrl.add(mUrl);
-            newsImage.add(mImage);
-
-        }
+    public void stopService() {
+        Intent serviceIntent = new Intent(this, ForegroundService.class);
+        stopService(serviceIntent);
     }
-    private void seventhResource(Document mBlogDocument){
-        Elements mElementDataSize = mBlogDocument.select("article[class=record article-summary]");
-        int mElementSize = mElementDataSize.size();
-
-        for (int i = 0; i < mElementSize; i++) {
-            Elements mNewsUrl = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mNewsTitle = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mElementImage = mBlogDocument.select("div[class=image]").select("img").eq(i);
-
-            String mTitle = mNewsTitle.attr("title");
-            String mUrl = mNewsUrl.attr("abs:href");
-            String mImage = mElementImage.attr("src");
-
-            newsTitle.add(mTitle);
-            newsUrl.add(mUrl);
-            newsImage.add(mImage);
-
-        }
-    }
-    private void eightResource(Document mBlogDocument){
-        Elements mElementDataSize = mBlogDocument.select("article[class=record article-summary]");
-        int mElementSize = mElementDataSize.size();
-
-        for (int i = 0; i < mElementSize; i++) {
-            Elements mNewsUrl = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mNewsTitle = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mElementImage = mBlogDocument.select("div[class=image]").select("img").eq(i);
-
-            String mTitle = mNewsTitle.attr("title");
-            String mUrl = mNewsUrl.attr("abs:href");
-            String mImage = mElementImage.attr("src");
-
-            newsTitle.add(mTitle);
-            newsUrl.add(mUrl);
-            newsImage.add(mImage);
-
-        }
-    }
-    private void ninethResource(Document mBlogDocument){
-        Elements mElementDataSize = mBlogDocument.select("article[class=record article-summary]");
-        int mElementSize = mElementDataSize.size();
-
-        for (int i = 0; i < mElementSize; i++) {
-            Elements mNewsUrl = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mNewsTitle = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mElementImage = mBlogDocument.select("div[class=image]").select("img").eq(i);
-
-            String mTitle = mNewsTitle.attr("title");
-            String mUrl = mNewsUrl.attr("abs:href");
-            String mImage = mElementImage.attr("src");
-
-            newsTitle.add(mTitle);
-            newsUrl.add(mUrl);
-            newsImage.add(mImage);
-
-        }
-    }
-    private void tenthResource(Document mBlogDocument){
-        Elements mElementDataSize = mBlogDocument.select("article[class=record article-summary]");
-        int mElementSize = mElementDataSize.size();
-
-        for (int i = 0; i < mElementSize; i++) {
-            Elements mNewsUrl = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mNewsTitle = mBlogDocument.select("div[class=image]").select("a").eq(i);
-            Elements mElementImage = mBlogDocument.select("div[class=image]").select("img").eq(i);
-
-            String mTitle = mNewsTitle.attr("title");
-            String mUrl = mNewsUrl.attr("abs:href");
-            String mImage = mElementImage.attr("src");
-
-            newsTitle.add(mTitle);
-            newsUrl.add(mUrl);
-            newsImage.add(mImage);
-
-        }
-    }
-    */
 }
